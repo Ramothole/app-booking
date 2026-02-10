e6ad3fa08511cd9ee5c0c02f9f4368d631d2c36d
Ramothole Kagiso <32358546+Ramothole@users.noreply.github.com>
2026-02-10 20:44:08 +0200

    Refactor Firebase integration: reorganized imports in index.html and created a new firebase.js file for Firebase configuration and service exports.

---
+++ js/firebase.js
@@ -0,0 +1,20 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  };
+// Initialize Firebase ONCE
+const app = initializeApp(firebaseConfig);
+
+// Export services
+export const db = getFirestore(app);
+export const storage = getStorage(app);

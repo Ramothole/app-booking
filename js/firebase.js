7b671bdfa222d012f8cc8b961fa1ebb3b8ddf869
Ramothole Kagiso <32358546+Ramothole@users.noreply.github.com>
2026-03-28 21:41:30 +0200

    Update Firebase configuration: Added complete Firebase project details to firebase.js, including measurement ID, and modified Firestore initialization to specify the project name for improved clarity and functionality.

--- js/firebase.js
+++ js/firebase.js
@@ -1,20 +1,18 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
-const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
-  };
-// Initialize Firebase ONCE
+export const firebaseConfig = {
+  apiKey: "AIzaSyD_GXP94lv9jkl_inXzX3tkeXHyJo1TOO8",
+  authDomain: "braided-society.firebaseapp.com",
+  projectId: "braided-society",
+  storageBucket: "braided-society.firebasestorage.app",
+  messagingSenderId: "521783048660",
+  appId: "1:521783048660:web:1b27a95f099f8c23847e41",
+  measurementId: "G-9JQ5H0DHML"
+};
 const app = initializeApp(firebaseConfig);
 
-// Export services
-export const db = getFirestore(app);
+
+export const db = getFirestore(app, "braided-society");
 export const storage = getStorage(app);
